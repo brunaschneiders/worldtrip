4ed3746eb67ddd14767c0d2c20ffd5bf491bf505
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-07-13 21:20:05 -0300

    add chakra ui and global styles

---
+++ styles/theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "700": "#47585B",
+      "500": "#999999",
+      "200": "#DADADA",
+      "100": "#F5F8FA",
+    },
+  },
+  fonts: {
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+        color: "gray.700",
+      },
+    },
+  },
+});
