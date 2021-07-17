6f9fc9c766fd7f0129c5db9204ac6950203c1997
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-07-16 22:09:58 -0300

    fix bannerHome

--- src/components/BannerHome/index.tsx
+++ src/components/BannerHome/index.tsx
@@ -14,33 +14,31 @@ export function BannerHome() {
       w="100%"
       px={["6", "20"]}
       py={["8", "12"]}
+      justifyContent="space-between"
     >
-      <Flex>
-        <Box maxW="524px">
-          <Text fontSize={["xl", "4xl"]} fontWeight="500" color="gray.200">
-            5 Continentes, <br />
-            infinitas possibilidades
-          </Text>
-          <Text
-            fontSize={["sm", "xl"]}
-            fontWeight="400"
-            color="gray.200"
-            mt={["2", "4"]}
-          >
-            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
-          </Text>
-        </Box>
-        {isWideVersion && (
-          <Image
-            src="/assets/airplane.png"
-            alt="airplane"
-            w="400px"
-            position="absolute"
-            right="0"
-            bottom="-10"
-          />
-        )}
-      </Flex>
+      <Box maxW="524px">
+        <Text fontSize={["xl", "4xl"]} fontWeight="500" color="gray.200">
+          5 Continentes, <br />
+          infinitas possibilidades
+        </Text>
+        <Text
+          fontSize={["sm", "xl"]}
+          fontWeight="400"
+          color="gray.200"
+          mt={["2", "4"]}
+        >
+          Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+        </Text>
+      </Box>
+      {isWideVersion && (
+        <Image
+          src="/assets/airplane.png"
+          alt="airplane"
+          w="400px"
+          position="absolute"
+          right="10"
+        />
+      )}
     </Flex>
   );
 }
